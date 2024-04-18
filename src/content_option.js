0c44928448391eee97ce56054067344793a1053e
Tapas Rout <52395178+tapas-artworld@users.noreply.github.com>
2024-04-18 11:51:22 +0530

    initial

---
+++ src/content_option.js
@@ -0,0 +1,193 @@
+const logotext = "TAPAS R";
+const meta = {
+    title: "Tapas Rout",
+    description: "I’m Tapas R",
+};
+
+const introdata = {
+    title: "I’m Tapas R",
+    animated: {
+        first: "Graphic Designer",
+        second: "UI UX Designer",
+        third: "I design mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Passionate and creative graphic, UI & UX designer, with 4 years of experience in the design industry working on a variety of projects from social media content to comprehensive rebranding projects. Work in two start ups. freelancing with three start ups company. First one is edu tech, second is e commerce and currently working in fintech. Once I discovered, that UX/UI Design is Absolutely my cup of tea. That’s why I’m keen on constantly developing my skills and becoming a professional.Sketching, painting, photography, reading books, listening to music, cycling, skating and playing BGMI are enhanced my creativity. Most of the time I live in my creative world !!",
+    
+};
+const worktimeline = [{
+        jobtitle: "UI | UX Designer",
+        where: "Gurugram",
+        date: "Mar 2023 - Present",
+    },
+    {
+        jobtitle: "Graphic Designer",
+        where: "Faridabad",
+        date: "Jul 2022 - Feb 2023",
+    },
+    {
+        jobtitle: "UI | UX and Graphic Designer",
+        where: "Dharamsala",
+        date: "Feb 2021 - Jun 2022",
+    },
+    {
+        jobtitle: "Graphic Designer intern",
+        where: "Dharamsala",
+        date: "Aug 2021 - Jan 2021",
+    },
+    {
+        jobtitle: "MERN Stack Dev",
+        where: "Dharamsala",
+        date: "May 2019 - Jul 2021",
+    },
+];
+
+const skills = [{
+        name: "Figma",
+        value: 90,
+    },
+    {
+        name: "Adobe XD",
+        value: 80,
+    },
+    {
+        name: "Photoshop",
+        value: 85,
+    },
+    {
+        name: "Illustor",
+        value: 80,
+    },
+    {
+        name: "blender",
+        value: 60,
+    },
+    {
+        name: "Jira",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "Product Designer at EazyHaina",
+        description: "Designing a Fintech Application called EazyHaina for Android, ISO UI | UX. Design creative for social media and Ads. working on EazyHaina branding, Application and website. EazyaHaina is a brand of wealthbird platform pvt. ltd. wealthbird in child Company of Quicksun. ",
+        skill: "Skills : • Prototyping • Sketch • Wireframing • Adobe Premiere Pro • Figma (Software) • Adobe Illustrator • Adobe Photoshop • User Flow • information architecture • Low fidelity • High fidelity.",
+    },
+    {
+        title: "UI & UX Designer at Quicksun Technologies",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Graphic Designer at Digital Impressions",
+        description: "In DI i'm working on multiple brands under one company, like Shroff eye centre gurgaon, Iyengaryogakshema, The post card, Octavius tea, Curio Cottage, Best Fit Universities, Design mailers, banners, advertisements creative for social media, videos, reels, motion graphics and Landing pages.",
+    },
+    {
+        title: "UI | UX and Graphic Designer at SayaCare",
+        description: "Brainstorming on the design of the concept & Develop graphics and info graphic designs for website, Phone UI and banners. Design brochures, logos, advertisements creative for social media, website UI, mobile UI and make ads for Promoting sayCare.",
+    },
+    {
+        title: "Graphic Designer intern at Pixel Maths",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "MERN Stack Dev at Navgurukul",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cdna.artstation.com/p/assets/images/images/057/724/204/large/tapas-r-tapas-artworld-89476311-616579288926333-6920654737675728139-n.jpg?1672470781",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdnb.artstation.com/p/assets/images/images/056/998/109/large/tapas-r-11.jpg?1670570990",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://scontent.fmaa1-1.fna.fbcdn.net/v/t39.30808-6/366194750_669264455222611_7205149924769192692_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=2iLwAh_31pIAb6WZ3iw&_nc_ht=scontent.fmaa1-1.fna&oh=00_AfBD0mt6P4yua52vnqQv-4-aGkeA_ly8icUZDAyAOVMNmA&oe=6625761E",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdnb.artstation.com/p/assets/images/images/057/724/211/large/tapas-r-tapas-artworld-90226510-1108867239450064-5807058693934993389-n.jpg?1672470792",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdna.artstation.com/p/assets/images/images/056/998/112/large/tapas-r-22.jpg?1670570994",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdna.artstation.com/p/assets/images/images/056/998/118/large/tapas-r-hulk.jpg?1670571003",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://cdnb.artstation.com/p/assets/images/images/057/724/317/large/tapas-r-group-427325954.jpg?1672471201",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdna.artstation.com/p/assets/images/images/056/998/122/large/tapas-r-ka.jpg?1670571010",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://media.licdn.com/dms/image/D5603AQHehAWwfcRtBA/profile-displayphoto-shrink_800_800/0/1705385401663?e=2147483647&v=beta&t=ltbbadQWkJAm0M0jHRJKZtoOJ0sxYGKXNnikf_dgoAA",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdna.artstation.com/p/assets/images/images/056/998/126/large/tapas-r-redgirl.jpg?1670571015",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdnb.artstation.com/p/assets/images/images/068/905/461/large/tapas-r-gg.jpg?1698899694",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://cdna.artstation.com/p/assets/images/images/056/998/114/large/tapas-r-33.jpg?1670570998",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/tapas-artworld",
+    facebook: "https://facebook.com/tapasartworld",
+    linkedin: "https://www.linkedin.com/in/tapas-artworld/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
